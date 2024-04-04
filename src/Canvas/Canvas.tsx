dd5c4e82e9327df577fcc88d329f357e11770f38
Anatoliy <tol.noskin@gmail.com>
2024-04-04 16:40:41 +0300

    move balls

--- src/Canvas/Canvas.tsx
+++ src/Canvas/Canvas.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, MouseEvent } from "react";
+import { useEffect, useRef, MouseEvent, useState } from "react";
 
 type buttonState = 'down' | 'up';
 
@@ -17,7 +17,9 @@ interface Speed {
   y: number;
 }
 
-interface Mouse extends Position {
+interface Mouse {
+  prevPos: Position
+  currentPos: Position
   buttonState: buttonState
 }
 
@@ -28,9 +30,15 @@ const Canvas = ({ width, height }: CanvasProps) => {
   const ballsRef = useRef<Ball[]>([]);
 
   let mouse : Mouse = {
-    x: width / 2,
-    y: height / 2,
-    buttonState: 'down',
+    prevPos: {
+      x: width / 2,
+      y: height / 2,
+    },
+    currentPos: {
+      x: width / 2,
+      y: height / 2,
+    },
+    buttonState: 'up',
   };
 
   class Ball {
@@ -39,16 +47,16 @@ const Canvas = ({ width, height }: CanvasProps) => {
     radius: number
     mass: number
     color: string
-    constructor() {
+    constructor({x, y}: Position) {
       this.pos = {
-        x: mouse.x,
-        y: mouse.y,
+        x: x,
+        y: y,
       }
       this.vel = {
         x: 0,
         y: 0,
       }
-      this.radius = 10
+      this.radius = 30
       this.mass = 0.02
       this.color = '#ff1100'
     }
@@ -65,10 +73,11 @@ const Canvas = ({ width, height }: CanvasProps) => {
   }
 
   function setPos(e: MouseEvent<HTMLCanvasElement>) {
-    [mouse.x, mouse.y] = [e.clientX, e.clientY];
+    [mouse.prevPos.x, mouse.prevPos.y] = [mouse.currentPos.x, mouse.currentPos.y];
+    [mouse.currentPos.x, mouse.currentPos.y] = [e.clientX, e.clientY];
   }
 
-  function updateBalls()  {
+/*   function updateBalls()  {
     for (let i = 0; i < ballsRef.current.length; i++) {
       let acceleration = {x: 0, y: 0};
       for (let j = 0; j < ballsRef.current.length; j++) {
@@ -86,9 +95,13 @@ const Canvas = ({ width, height }: CanvasProps) => {
       ballsRef.current[i].vel.x = ballsRef.current[i].vel.x * 0.98 + acceleration.x * ballsRef.current[i].mass;
       ballsRef.current[i].vel.y = ballsRef.current[i].vel.y * 0.98 + acceleration.y * ballsRef.current[i].mass;
     }
-  }
+  } */
 
   function isDown() {
+    mouse.buttonState = mouse.buttonState === 'up' ? 'down' : 'up';
+  }
+
+  function isUp() {
     mouse.buttonState = mouse.buttonState === 'down' ? 'up' : 'down';
   }
 
@@ -98,11 +111,23 @@ const Canvas = ({ width, height }: CanvasProps) => {
         context.fillStyle = '#03002E';
         context.fillRect(0, 0, width, height);
 
-        if (mouse.buttonState === 'up') {
-          ballsRef.current.push(new Ball())
+        if (mouse.buttonState === 'down') {
+          const movedBall = ballsRef.current.find((el) => {
+            const maxElPosX = el.pos.x + el.radius;
+            const minElPosX = el.pos.x - el.radius;
+            const maxElPosY = el.pos.y + el.radius;
+            const minElPosY = el.pos.y - el.radius;
+
+            return mouse.currentPos.x >= minElPosX && mouse.currentPos.x <= maxElPosX && mouse.currentPos.y >= minElPosY && mouse.currentPos.y <= maxElPosY;
+          });
+
+          if (movedBall) {
+            movedBall.pos.x = mouse.currentPos.x;
+            movedBall.pos.y = mouse.currentPos.y;
+          }
         }
 
-        updateBalls();
+        /* updateBalls(); */
         ballsRef.current.map(e => e.draw(context));
 
         frameRef.current = requestAnimationFrame(() => {
@@ -113,7 +138,25 @@ const Canvas = ({ width, height }: CanvasProps) => {
 
     if (ref.current) {
       const context = ref.current.getContext('2d');
-      ballsRef.current = [];
+      ballsRef.current = [new Ball({x: width / 2 , y: height / 2})];
+      for (let i = 1; i <= 5; i++) {
+        ballsRef.current.push(
+          new Ball(
+            {
+              x: ballsRef.current[ballsRef.current.length - i].pos.x - ballsRef.current[ballsRef.current.length - i].radius,
+              y: ballsRef.current[ballsRef.current.length - i].pos.y - ballsRef.current[ballsRef.current.length - i].radius * Math.sqrt(3)}
+            )
+          )
+        for (let j = 0; j < i ; j++) {
+          ballsRef.current.push
+          (new Ball(
+            {
+              x: ballsRef.current[ballsRef.current.length - 1].pos.x + 2 * ballsRef.current[ballsRef.current.length - 1].radius,
+              y: ballsRef.current[ballsRef.current.length - 1].pos.y
+            })
+          )
+        }
+      }
 
       if (context) {
         context.canvas.width = width;
@@ -128,7 +171,7 @@ const Canvas = ({ width, height }: CanvasProps) => {
     return () => cancelAnimationFrame(frameRef.current);
   }, [height, width]);
 
-  return <canvas onMouseMove={setPos} onMouseUp={isDown} onMouseDown={isDown} ref={ref} width={width} height={height}/>;
+  return <canvas onMouseMove={setPos} onMouseUp={isUp} onMouseDown={isDown} ref={ref} width={width} height={height}/>;
 };
 
 Canvas.defaultProps = {
